e8df735dcdd540a91b002d3c6c9447801e40c42f
RobertoFORTs <bobfortesneto@gmail.com>
2023-10-11 14:38:09 -0400

    a

--- src/config/axios-config.ts
+++ src/config/axios-config.ts
@@ -1,8 +1,43 @@
 import axios from 'axios';
 
-export const client = axios.create({
+// export const getAuthTokenUserLogged = (): string => {
+//   try {
+//     let jwtUser = '';
+//     const authUserLogged = getAuthUserLogged();
+//     if (authUserLogged && authUserLogged.authenticationToken) {
+//       jwtUser = authUserLogged.authenticationToken;
+//     }
+//     return jwtUser;
+//   } catch (error: any) {
+//     if (error.response && error.response.status === 401) {
+//       window.location.href = '/login';
+//     } 
+//     return '';
+//   }
+// }
+
+export const getAuthUserLogged = (): string => {
+  try {
+    let authUserLogged: string = "";
+    if (localStorage.getItem('auth')){
+      authUserLogged = JSON.parse(localStorage.getItem('auth')!);
+    }
+    console.log("Error do token mal formado", authUserLogged);
+    return authUserLogged;
+  } catch (error: any) {
+    if (error.response && error.response.status === 401) {
+      window.location.href = '/login';
+    } 
+    console.log("Error 2 do token mal formado");
+
+    return "Not";
+  }
+}
+
+export const client = axios.create({  
   baseURL: process.env.REACT_APP_API_BASE_ADDR,
   headers: {
     'Content-Type': 'application/json',
+    'Authorization': `Bearer ${getAuthUserLogged()}`
   },
 });
