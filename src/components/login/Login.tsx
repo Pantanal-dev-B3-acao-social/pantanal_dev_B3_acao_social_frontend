da72297b3cfaa3ee6f7fb1fe2eef3417ecdec063
RobertoFORTs <bobfortesneto@gmail.com>
2023-10-11 10:25:46 -0400

    feat: update css style for login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -62,33 +62,32 @@ const Login: FC = () => {
 
   return (
     <>
-      <div className="container">
+<div className="container">
   <div className="row">
     <div className="col-md-6 offset-md-3">
       <div className="card">
-        <div className="card-body">
-          <h3 className="card-title text-center mb-4">Login</h3>
-          <form autoComplete="off" onSubmit={handleSubmit(login)}>
-            <div className="form-group">
-              <label htmlFor="username" className="form-label">Username</label>
-              <input type="text" id="username" className="form-control" {...register("username", { required: "Username is required!" })} />
-              {errors.username && <p className="text-danger">{errors.username.message}</p>}
-            </div>
-            <div className="form-group">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" id="password" className="form-control" {...register("password", { required: "Password is required!" })} />
-              {errors.password && <p className="text-danger">{errors.password.message}</p>}
-            </div>
-            <div className="text-center mt-4">
-              <button type="submit" className="btn btn-primary btn-lg">Submit</button>
-              <p className="card-text mt-3">Don't have an account? <Link to="/register">Sign Up</Link></p>
-            </div>
-          </form>
-        </div>
+        <h3 className="card-title text-center mb-4">Login</h3>
+        <form autoComplete="off" onSubmit={handleSubmit(login)}>
+          <div className="form-group">
+            <label htmlFor="username" className="form-label">Username</label>
+            <input type="text" id="username" className="form-control" {...register("username", { required: "Username is required!" })} />
+            {errors.username && <p className="text-danger">{errors.username.message}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="password" id="password" className="form-control" {...register("password", { required: "Password is required!" })} />
+            {errors.password && <p className="text-danger">{errors.password.message}</p>}
+          </div>
+          <div className="text-center mt-4">
+            <button type="submit" className="btn btn-primary btn-lg">Submit</button>
+            <p className="card-text mt-3">Don't have an account? <Link to="/register">Sign Up</Link></p>
+          </div>
+        </form>
       </div>
     </div>
   </div>
 </div>
+
       <ToastContainer
         position="top-right"
         autoClose={5000}
