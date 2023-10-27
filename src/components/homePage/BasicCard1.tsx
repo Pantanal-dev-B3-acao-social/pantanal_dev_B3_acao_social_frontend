9c9b95d6c5a05867f295afcb62d3f5cbe4ac7163
VictorManuelChang <victor.manuel@ufms.br>
2023-10-27 09:07:53 -0400

    imagens

--- src/components/homePage/BasicCard1.tsx
+++ src/components/homePage/BasicCard1.tsx
@@ -1,40 +1,27 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
 
-export default function BasicCard() {
+
+export default function ImgMediaCard() {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ maxWidth: 345 }}>
+      <img src={require("/public/logo512.png").default} alt="Descrição da imagem" style={{ maxWidth: '100%', height: 'auto' }} />
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
         </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
         </Typography>
       </CardContent>
       <CardActions>
+        <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
